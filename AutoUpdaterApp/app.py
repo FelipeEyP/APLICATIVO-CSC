c5659ffd9c9dbf76301733d741beeb26d0bcc401
FelipeEyP <felipe@example.com>
2026-05-28 14:33:36 -0500

    Cambios

--- AutoUpdaterApp/app.py
+++ AutoUpdaterApp/app.py
@@ -9,7 +9,7 @@ import tempfile
 import urllib.request
 
 # --- CONFIGURACIÓN DE LA APP ---
-APP_VERSION = "1.0.0" # Actualizamos para forzar un nuevo build
+APP_VERSION = "1.0.1" # Actualizamos para forzar un nuevo build
 UPDATE_URL = "https://raw.githubusercontent.com/FelipeEyP/APLICATIVO-CSC/main/AutoUpdaterApp/version.json"
 
 class ModernApp(ctk.CTk):
